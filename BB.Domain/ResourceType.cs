06929d54c808b64fe69e8cff72aeac3e52801322
Ste Prescott <mail@ste.me>
2015-01-11 21:02:00 +0000

    Created domain object

---
+++ BB.Domain/ResourceType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BB.Domain
+{
+    public class ResourceType
+    {
+        public Guid ResourceTypeID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<Guid> ResourceIDs { get; set; }
+    }
+}
