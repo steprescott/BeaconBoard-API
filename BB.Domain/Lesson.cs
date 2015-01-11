f81f26a9d44ccd84296dbac2eab01d257fd2f39c
Ste Prescott <mail@ste.me>
2015-01-11 19:51:26 +0000

    Lesson domain object

---
+++ BB.Domain/Lesson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BB.Domain
+{
+    public class Lesson
+    {
+        public Guid LessonID { get; set; }
+
+        public List<Guid> SessionIDs { get; set; }
+        public List<Guid> ResourceIDs { get; set; }
+        public List<Guid> CourseIDs { get; set; }
+    }
+}
